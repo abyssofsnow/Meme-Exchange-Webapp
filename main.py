02f2f785fa812c24003bd8524cad88da6a4d0388
Emily Higgs <emilycarolineh@gmail.com>
2020-02-09 19:53:55 -0500

    infrastructure

---
+++ main.py
@@ -0,0 +1,26 @@
+	from flask import Flask
+	app = Flask(__name__)
+	
+		
+	# The home page handler
+	@app.route('/')
+	def navigate_home():
+		return "Homepage"
+	
+	# The login/registration page handler
+	@app.route('/login')
+	def navigate_login():
+		return "Login Page"
+
+	@app.route('/user/<username>')
+	def navigate_user_page():
+		return "User Page"
+
+	@app.route('meme/<meme_id>')
+	def navigate_meme_page():
+		return "Meme Page"
+
+
+	if __name__ == '__main__':
+		app.run()
+
