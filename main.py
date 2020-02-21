0cff7ae19937cf8e1939b75eef64447dc41fba1f
KyleHYX <60859865+KyleHYX@users.noreply.github.com>
2020-02-21 15:10:17 -0500

    remove blank spaces in main.py
    remove blank spaces in main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-	from flask import Flask
-	app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
+
 	
-		
-	# The home page handler
-	@app.route('/')
-	def navigate_home():
-		return "Homepage"
-	
-	# The login/registration page handler
-	@app.route('/login')
-	def navigate_login():
-		return "Login Page"
+# The home page handler
+@app.route('/')
+def navigate_home():
+	return "Homepage"
 
-	@app.route('/user/<username>')
-	def navigate_user_page():
-		return "User Page"
+# The login/registration page handler
+@app.route('/login')
+def navigate_login():
+	return "Login Page"
 
-	@app.route('meme/<meme_id>')
-	def navigate_meme_page():
-		return "Meme Page"
+@app.route('/user/<username>')
+def navigate_user_page():
+	return "User Page"
+
+@app.route('meme/<meme_id>')
+def navigate_meme_page():
+	return "Meme Page"
 
 
-	if __name__ == '__main__':
-		app.run()
+if __name__ == '__main__':
+	app.run()
 
