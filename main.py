1d1285b281d9ec095736e14744bbc10e5083a305
Emily Higgs <emilycarolineh@gmail.com>
2020-02-27 18:11:07 -0500

    n

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ CLOUD_STORAGE_BUCKET = os.environ['CLOUD-STORAGE-BUCKET']
 # new user creation from login page
 @app.route('/createuser/<newUser>/<UserName>', methods=['POST'])
 def createuser(newUser, UserName):
-    entity = datastore.Entity(key=datastore_client.key('User', newUser))
-    entity.update({
-        'username': UserName,
-        'bio': '',
-        'friend_request_in': [],
-        'friends': [],
-        'memes': [],
-        'picture': '',
-    })
-    datastore_client.put(entity)
-    return console.log('Create User Success!')
+	entity = datastore.Entity(key=datastore_client.key('User', newUser))
+	entity.update({
+		'username': UserName,
+		'bio': '',
+		'friend_request_in': [],
+		'friends': [],
+		'memes': [],
+		'picture': '',
+	})
+	datastore_client.put(entity)
+	return console.log('Create User Success!')
 
 
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+	return render_template('signup.html')
 
 
 # The home page handler
@@ -56,7 +56,7 @@ def go_popularMeme():
 # The login/registration page handler
 @app.route('/login')
 def navigate_login():
-    return render_template('login.html')
+	return render_template('login.html')
 
 
 @app.route('/user/<username>', methods=['GET'])
@@ -64,102 +64,102 @@ def navigate_user_page(username):
 
 	identity = ''
 
-    # Verify Firebase Auth.
+	# Verify Firebase Auth.
 	id_token = request.cookies.get("token")
 
 	if id_token:
-        try:
-            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+		try:
+			claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
 
 			# Load the datastore object for the user visiting the page.
-            key = datastore_client.key('User', claims['email'])
-            user_self = datastore_client.get(key)
+			key = datastore_client.key('User', claims['email'])
+			user_self = datastore_client.get(key)
 
-            # If the page visitor is logged in, store their identity.
-            if(user_self is not None):
-                identity = user_self['username']
-            else:
-                identity = ''
-        except ValueError as exc:
-            error_message = str(exc)
+			# If the page visitor is logged in, store their identity.
+			if(user_self is not None):
+				identity = user_self['username']
+			else:
+				identity = ''
+		except ValueError as exc:
+			error_message = str(exc)
 
-    # Query for the user whose profile we're looking for.
-    query = datastore_client.query(kind='User')
-    query.add_filter('username', '=', username)
-    user_viewed_list = list(query.fetch())
+	# Query for the user whose profile we're looking for.
+	query = datastore_client.query(kind='User')
+	query.add_filter('username', '=', username)
+	user_viewed_list = list(query.fetch())
 
-    # If the user is found, use its data to populate the profile.
-    if(len(user_viewed_list) > 0):
+	# If the user is found, use its data to populate the profile.
+	if(len(user_viewed_list) > 0):
 
-        user_viewed = user_viewed_list[0]
+		user_viewed = user_viewed_list[0]
 
-        # Load the memes associated with the user to be displayed. 
-        query = datastore_client.query(kind='Meme')
-        query.add_filter('owner', '=', username)
-        memes = list(query.fetch())
+		# Load the memes associated with the user to be displayed. 
+		query = datastore_client.query(kind='Meme')
+		query.add_filter('owner', '=', username)
+		memes = list(query.fetch())
 
-        return render_template('profile.html', user_to_display=user_viewed, safe_user_to_display=json.dumps(user_viewed), memes_owned=memes, identity=identity)
-    else:
-        return "User not found"
+		return render_template('profile.html', user_to_display=user_viewed, safe_user_to_display=json.dumps(user_viewed), memes_owned=memes, identity=identity)
+	else:
+		return "User not found"
 
 
 @app.route('/user/<username>', methods=['POST'])
 def update_user_page(username):
-    return ""
+	return ""
 
 
 @app.route('/sendFriendRequest', methods=['POST'])
 def send_friend_request():
-    sender = request.json['sender']
-    receiver = request.json['receiver']
+	sender = request.json['sender']
+	receiver = request.json['receiver']
 
 
-    # Check that this user hasn't already received a friend request from recip.
-    query = datastore_client.query(kind='User')
-    query.add_filter('username', '=', sender)
-    sender_objects = list(query.fetch())
+	# Check that this user hasn't already received a friend request from recip.
+	query = datastore_client.query(kind='User')
+	query.add_filter('username', '=', sender)
+	sender_objects = list(query.fetch())
 
-    sender_obj = sender_objects[0]
-    for friend_request in sender_obj['friend_request_in']:
-        if(friend_request == receiver):
-            return json.dumps({'success': False}), 403, {'ContentType': 'application/json'}
+	sender_obj = sender_objects[0]
+	for friend_request in sender_obj['friend_request_in']:
+		if(friend_request == receiver):
+			return json.dumps({'success': False}), 403, {'ContentType': 'application/json'}
 
-    query = datastore_client.query(kind='User')
-    query.add_filter('username', '=', receiver)
-    receiver_objects = list(query.fetch())
+	query = datastore_client.query(kind='User')
+	query.add_filter('username', '=', receiver)
+	receiver_objects = list(query.fetch())
 
 
-    receiver_obj = receiver_objects[0]
-    # Return error if this user already sent friend request.
-    for friend_request in receiver_obj['friend_request_in']:
-        if(friend_request == sender):
-            return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
+	receiver_obj = receiver_objects[0]
+	# Return error if this user already sent friend request.
+	for friend_request in receiver_obj['friend_request_in']:
+		if(friend_request == sender):
+			return json.dumps({'success': False}), 400, {'ContentType': 'application/json'}
 
-    # Return error if they are already friends.
-    for friend_entry in sender_obj['friends']:
-        if(friend_request == receiver):
-            return json.dumps({'success': False}), 401, {'ContentType': 'application/json'}
+	# Return error if they are already friends.
+	for friend_entry in sender_obj['friends']:
+		if(friend_request == receiver):
+			return json.dumps({'success': False}), 401, {'ContentType': 'application/json'}
 
-    # Add a friend request between these two.
-    try:
-        with datastore_client.transaction():
-            key = datastore_client.key('User', receiver)
-            task = datastore_client.get(key)
+	# Add a friend request between these two.
+	try:
+		with datastore_client.transaction():
+			key = datastore_client.key('User', receiver)
+			task = datastore_client.get(key)
 
-            # Add the sender to the receiver's pending invites
-            task['friend_request_in'] = receiver_obj['friend_request_in'].append(sender)
+			# Add the sender to the receiver's pending invites
+			task['friend_request_in'] = receiver_obj['friend_request_in'].append(sender)
 
-            datastore_client.put(task)
+			datastore_client.put(task)
 
-    except:
-        return json.dumps({'success': False}), 500, {'ContentType': 'application/json'} 
+	except:
+		return json.dumps({'success': False}), 500, {'ContentType': 'application/json'} 
 
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'} 
+	return json.dumps({'success': True}), 200, {'ContentType': 'application/json'} 
 
 """
 @app.route('/uploadImage', methods=['POST'])
 def upload(image):
-    	#processing and uploading image to GCS
+		#processing and uploading image to GCS
 	uploadedImg = request.files.get('file')
 
 	if not uploadedImg:
@@ -184,24 +184,24 @@ def upload(image):
 
 @app.route('/meme/<meme_id>', methods=['GET'])
 def navigate_meme_page(meme_id):
-    # Load the datastore object for the meme.
-    query = datastore_client.query(kind='Meme')
-    query.add_filter('key', '=', meme_id)
-    memes = list(query.fetch())
+	# Load the datastore object for the meme.
+	query = datastore_client.query(kind='Meme')
+	query.add_filter('key', '=', meme_id)
+	memes = list(query.fetch())
 
-    if(len(memes) > 0):
-        render_template('meme.html', meme_to_display=memes[0])
-    else:
-        return "Meme not found"
+	if(len(memes) > 0):
+		render_template('meme.html', meme_to_display=memes[0])
+	else:
+		return "Meme not found"
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error has occurred')
-    return """
-    An internal error has occurred: <pre>{}</pre>
-    """.format(e), 500
+	logging.exception('An error has occurred')
+	return """
+	An internal error has occurred: <pre>{}</pre>
+	""".format(e), 500
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
 
 
